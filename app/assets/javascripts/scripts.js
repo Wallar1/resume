c830da0922b3316b764b7993cddcaa26d2556271
Craig Wallace <craig.wallace@sungrade.solar>
2018-07-10 17:57:59 -0700

    Adding some scrolling animations

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,21 +1,52 @@
 jQuery(function ($) {
 
-    'use strict';
+  'use strict';
+  
+  // --------------------------------------------------------------------
+  // PreLoader
+  // --------------------------------------------------------------------
 
-    // --------------------------------------------------------------------
-    // PreLoader
-    // --------------------------------------------------------------------
+  (function () {
+    $('#preloader').delay(200).fadeOut('slow');
+  }());
 
-    (function () {
-        $('#preloader').delay(200).fadeOut('slow');
-    }());
+  
 
 
+  // --------------------------------------------------------------------
+  // Sticky Sidebar
+  // --------------------------------------------------------------------
 
-    // --------------------------------------------------------------------
-    // Sticky Sidebar
-    // --------------------------------------------------------------------
+  $('.left-col-block, .right-col-block').theiaStickySidebar();
 
-    $('.left-col-block, .right-col-block').theiaStickySidebar();
+
+  //----------------------------------------------------------------------
+  // Scrolling Animations
+  // found at: https://codepen.io/bramus/pen/yikfd
+  //----------------------------------------------------------------------
+
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+      $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+    $animatables.each(function(i) {
+      var $animatable = $(this);
+      if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+      }
+    });
+  };
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+  $(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
 
 }); // JQuery end
